600db30e468e3fd6474c0d5e69a84496cea61d85
Michael Toyi <michael.toyi@norrenberger.local>
2024-07-04 13:19:18 +0100

    Required endpoints update

--- app.js
+++ app.js
@@ -24,7 +24,40 @@ const {
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
+
 // Your code here
+app.get('/artists',(req,res) => {
+  try{
+    const allArtists = getAllArtists();
+
+  return res.status(200).json({allArtists})
+}catch(error){
+  console.log(error)
+
+  return res.status(500).send(error)
+}
+})
+
+app.post('/artists', (req,res)=> {
+  try{
+    const {name} = req.body;
+  
+  const addArtiste = addArtist(name);
+
+  return res.status(201).json({addArtiste})
+}catch(error){
+  console.log(error)
+
+  return res.status(500).send(error)
+}
+})
+
 
 // DO NOT MODIFY
 if (require.main === module) {
